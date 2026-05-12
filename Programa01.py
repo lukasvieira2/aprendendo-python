94cd66648826bcc77450bd4dbfe9cdbfde79317c
lukasvieira2 <=>
2026-05-12 17:40:07 -0300

    meus primeiros codigo em python

---
+++ Programa01.py
@@ -0,0 +1,30 @@
+
+numero1 = float(input("Digite o primeiro número: "))
+numero2 = float(input("Digite o segundo número: "))
+numero3 = float(input("Digite o segundo número: "))
+numero4 = float(input("Digite o segundo número: "))
+
+soma = numero1 + numero2 + numero3 + numero4
+
+Total = soma / 2
+
+print("A divisão é:",Total)
+
+nome = "maria"
+idade = 20
+peso = 62.5
+menoridade = True
+
+print("essa é a ", nome,"ela tem ", idade," anos e pesa", peso,"kg")
+print("O nome dela é "+ nome + " eu encontrei ela no tinder")
+print("hoje é o aniversário da {} e ela está completando {} anos e pesa {} kg  alem disso ela é maior de idade: {}".format(nome , idade, peso, menoridade))
+
+# print("hello world!")
+# print("1") #str (string de caractere)
+# print(1) # int (numero inteiro)
+# print(-2) # int (numero inteiro)
+# print(8.2) # float (numero real - decimal, ponto flutuante)
+# print(true) # Verdadeiro (booleano - bool)
+# print(false) # Falso (boleano - bool)
+# print("if") # Ele verifica se uma condição é verdadeira (True) ou falsa (False).
+# print("else") # Se a condição do if não for verdadeira, faça outra coisa
---
+++ exercicio01.py
@@ -0,0 +1,7 @@
+numero1 = 21
+numero2 = 23
+
+soma = numero1 + numero2 
+
+
+print("A soma é:",soma)
