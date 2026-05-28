2a02630f509f3ccf3a1dd7f2743b2a4d03ed83a0
lukasvieira2 <=>
2026-05-28 17:18:37 -0300

    meus primeiros codigo em python

--- for/exercicio11.py
+++ for/exercicio11.py
@@ -1,8 +1,8 @@
 
-#Exercício 11: O Caixa Eletrônico / Saque de Cédulas (while)
+#Exercício 11: O Caixa Eletrônico / saque de Cédulas (while)
 #Enunciado: Crie um programa que simule o funcionamento de um caixa eletrônico. O usuário informa o valor que deseja sacar (número inteiro). O programa deve calcular quantas cédulas de cada valor serão entregues, priorizando as maiores. Considere que o banco possui cédulas de R$ 50, R$ 20, R$ 10 , 5 R$ e R$ 2.
 
-#Exemplo: Saque de R$ 82 ➔ 1 cédula de R$ 50, 1 de R$ 20, 1 de R$ 10 e 1 de R$ 2.
+#Exemplo: saque de R$ 82 ➔ 1 cédula de R$ 50, 1 de R$ 20, 1 de R$ 10 e 1 de R$ 2.
 
 valor = int(input("Digite o valor do saque: R$ "))
 
@@ -14,7 +14,10 @@ while i < len(cedulas):
     nota = cedulas[i]
     quantidade = valor // nota
 
-    if quantidade > 0:
+    if  quantidade == 5 and (valor == 6 or valor == 8 or valor == 11):
+        continue
+
+    if  quantidade > 0:
         print(f"{quantidade} cédula(s) de R$ {nota}")
 
     valor = valor % nota
---
+++ for/exercicio17.py
@@ -0,0 +1,4 @@
+# Exercício 17: A Tabuada Automatizada
+#Enunciado: Desenvolva um programa que peça para o usuário digitar um número inteiro.
+# Utilizando o laço for e a função range(), exiba a tabuada desse número de 1 a 10 no terminal.
+#Exemplo de saída esperada: 5 x 1 = 5, 5 x 2 = 10...
