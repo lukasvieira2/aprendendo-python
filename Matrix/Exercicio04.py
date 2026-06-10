6b219c6b843e32eb90d51cd3e7c44ce9c4ad1eb6
lukasvieira2 <=>
2026-06-10 14:14:18 -0300

    meus primeiros codigo em python

--- Matrix/Exercicio04.py
+++ Matrix/Exercicio04.py
@@ -5,13 +5,13 @@ vendas = [
     [700, 600, 800, 900]
 ]
 
-# Total vendido por cada vendedor
+
 print("Total vendido por cada vendedor:")
 for i in range(len(vendas)):
     total_vendedor = sum(vendas[i])
     print(f"Vendedor {i+1}: R$ {total_vendedor}")
 
-# Total vendido por dia
+
 print("\nTotal vendido por dia:")
 for dia in range(len(vendas[0])):
     total_dia = 0
---
+++ Matrix/Exercicio10.py
@@ -0,0 +1,18 @@
+notas = [
+    [7.0, 8.5, 6.0, 7.5],
+    [9.0, 9.5, 10.0, 8.5],
+    [5.0, 6.0, 5.5, 4.0]
+]
+
+medias = []
+
+# Calculando a média de cada linha (aluno)
+for linha in notas:
+    soma_notas = sum(linha)
+    quantidade_notas = len(linha)
+    media_aluno = soma_notas / quantidade_notas
+    medias.append(media_aluno)
+
+# Exibindo as médias
+for i, media in enumerate(medias):
+    print(f"Média do Aluno {i + 1}: {media:.2f}")
